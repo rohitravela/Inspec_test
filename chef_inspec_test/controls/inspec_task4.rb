54d3382dbf45d807e3291d45712642fe43deb0a9
rohitravela <55970740+rohitravela@users.noreply.github.com>
2020-05-19 20:47:58 +0530

    Update inspec_task4.rb

--- chef_inspec_test/controls/inspec_task4.rb
+++ chef_inspec_test/controls/inspec_task4.rb
@@ -1,4 +1,4 @@
-control "CIS standard - 1" do      # Checks if the GPG key is installed               
+control "CIS standard - 1" do      # Checks if the GPG key is installed or not              
     impact 1.0                                
     title "1.2.1 Verify CentOS GPG Key is Installed (Scored)"
     desc "CentOS cryptographically signs updates with a GPG key to verify that they are valid."
